c0c733f317f538e828bfe21f4347e69b08452f55
NotStatilko <thenonproton@protonmail.com>
2023-03-22 23:40:01 +0200

    Add urllib3 to setup.py & optional fast tgbox

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ setup(
     py_modules=['tgbox_cli','tools'],
 
     install_requires=[
-        'tgbox', 'click==8.1.3',
+        'tgbox', 'urllib3',
+        'click==8.1.3',
         'enlighten==1.10.2'
     ],
+    extras_require = {
+        'fast': ['tgbox[fast]']
+    },
     entry_points='''
         [console_scripts]
         tgbox-cli=tgbox_cli:safe_cli
