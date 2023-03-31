86de7777061112a3cf55665f1bc69878df463bc3
NotStatilko <thenonproton@protonmail.com>
2023-03-31 23:48:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 setup(
     name="tgbox-cli",
     version='1.0',
-    py_modules=['tgbox_cli','tools'],
+    packages=['tgbox_cli'],
 
     install_requires=[
         'tgbox', 'urllib3',
         'click==8.1.3',
         'enlighten==1.10.2'
     ],
-    extras_require = {
+    extras_require={
         'fast': ['tgbox[fast]']
     },
     entry_points='''
         [console_scripts]
-        tgbox-cli=tgbox_cli:safe_cli
+        tgbox-cli=tgbox_cli.tgbox_cli:safe_cli
     ''',
 )
