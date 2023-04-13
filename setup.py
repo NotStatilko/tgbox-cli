00383a18984444b018aa97ed9d779a32a495dfb1
NotStatilko <thenonproton@protonmail.com>
2023-04-14 01:56:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,22 @@ setup(
     version='1.0',
     packages=['tgbox_cli'],
 
+    package_data = {
+        'tgbox_cli': ['tgbox_cli/data'],
+    },
+    include_package_data = True,
+
     install_requires=[
-        'tgbox', 'urllib3',
+        'urllib3',
+        'tgbox<2',
         'click==8.1.3',
         'enlighten==1.10.2'
     ],
     extras_require={
-        'fast': ['tgbox[fast]']
+        'fast': ['tgbox[fast]<2']
     },
     entry_points='''
         [console_scripts]
-        tgbox-cli=tgbox_cli.tgbox_cli:safe_cli
-    ''',
+        tgbox-cli=tgbox_cli.tgbox_cli:safe_tgbox_cli_startup
+    '''
 )
