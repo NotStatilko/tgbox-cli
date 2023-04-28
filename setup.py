368073fcad90128dfc45c0ec16687b673acf0df3
NotStatilko <thenonproton@protonmail.com>
2023-04-28 23:33:23 +0300

    Update setup.py file

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup
+from ast import literal_eval
+
+with open('tgbox_cli/version.py', encoding='utf-8') as f:
+    version = literal_eval(f.read().split('=')[1].strip())
 
 setup(
-    name="tgbox-cli",
-    version='1.0',
-    packages=['tgbox_cli'],
+    name             = "tgbox-cli",
+    version          = version,
+    packages         = ['tgbox_cli'],
+    license          = 'MIT',
+    description      = 'A Command Line Interface to the TGBOX',
+    long_description = open('README.md', encoding='utf-8').read(),
+    author           = 'NotStatilko',
+    author_email     = 'thenonproton@pm.me',
+    url              = 'https://github.com/NotStatilko/tgbox-cli',
+    download_url     = f'https://github.com/NotStatilko/tgbox-cli/archive/refs/tags/v{version}.tar.gz',
+
+    long_description_content_type='text/markdown',
 
     package_data = {
         'tgbox_cli': ['tgbox_cli/data'],
@@ -19,6 +32,21 @@ setup(
     extras_require={
         'fast': ['tgbox[fast]<2']
     },
+    keywords = [
+        'Telegram', 'Cloud-Storage',
+        'Cloud', 'Non-official'
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
     entry_points='''
         [console_scripts]
         tgbox-cli=tgbox_cli.tgbox_cli:safe_tgbox_cli_startup
