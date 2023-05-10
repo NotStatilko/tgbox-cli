c8d121e2c5afd6ca277681140f3224dc5b6aee09
NotStatilko <thenonproton@protonmail.com>
2023-05-10 13:01:16 +0300

    file-forward by filters

--- tgbox_cli/tgbox_cli.py
+++ tgbox_cli/tgbox_cli.py
@@ -2271,35 +2271,103 @@ def file_open(filters, locate, propagate, continuously):
     raise ExitProgram
 
 @cli.command()
-@click.argument('entity', nargs=-1)
+@click.argument('filters', nargs=-1)
 @click.option(
-    '--id', required=True, type=int,
-    help='File ID to forward'
+    '--entity', '-e', required=True,
+    help='Entity to send file to'
 )
-def file_forward(entity, id):
+def file_forward(filters, entity):
     """
     Will forward file to specified entity
 
     \b
+    Available filters:\b
+        scope: Define a path as search scope
+               -----------------------------
+               The *scope* is an absolute directory in which
+               we will search your file by other filters. By
+               default, the tgbox.api.utils.search_generator
+               will search over the entire LocalBox. This can
+               be slow if you're have too many files.
+               \b
+               Example: let's imagine that You're a Linux user which
+               share it's Box with the Windows user. In this case,
+               Your LocalBox will contain a path parts on the
+               '/' (Linux) and 'C:\\' (Windows) roots. If You
+               know that some file was uploaded by Your friend,
+               then You can specify a scope='C:\\' to ignore
+               all files uploaded from the Linux machine. This
+               will significantly fasten the search process,
+               because almost all filters require to select
+               row from the LocalBox DB, decrypt Metadata and
+               compare its values with ones from SearchFilter.
+               \b
+               !: The scope will be ignored on RemoteBox search.
+               !: The min_id & max_id will be ignored if scope used.
+        \b
+        id integer: File’s ID
+        mime str: File mime type
+        \b
+        cattrs: File CAttrs
+                -----------
+                Can be used hexed PackedAttributes
+                or special CLI format alternatively:
+                cattrs="comment:test type:message"
+        \b
+        file_path str: File path
+        file_name str: File name
+        file_salt str: File salt
+        \b
+        min_id integer: File ID should be > min_id
+        max_id integer: File ID should be < max_id
+        \b
+        min_size integer: File Size should be > min_size
+        max_size integer: File Size should be < max_size
+        \b
+        min_time integer/float: Upload Time should be > min_time
+        max_time integer/float: Upload Time should be < max_time
+        \b
+        imported bool: Yield only imported files
+        re       bool: re_search for every bytes filter
+    \b
+    See tgbox.readthedocs.io/en/indev/
+        tgbox.html#tgbox.tools.SearchFilter
+    \b
+    You can also use special flags to specify
+    that filters is for include or exclude search.
+    \b
     Example:\b
-        tgbox-cli file-forward @username --id=161
-        tgbox-cli file-forward me @channel --id=22
+        # Include is used by default
+        tgbox-cli file-forward scope=/home/non/Documents -e @username
+        \b
+        # Include flag (will ignore unmatched)
+        tgbox-cli file-forward +i id=22 --entity me
+        \b
+        # Exclude flag (will ignore matched)
+        tgbox-cli file-forward +e mime=audio --entity @channel
+        \b
+        You can use both, the ++include and
+        ++exclude (+i, +e) in one command.
     """
     dlb, drb = get_box()
+    try:
+        sf = filters_to_searchfilter(filters)
+    except IndexError: # Incorrect filters format
+        echo('[RED]Incorrect filters! Make sure to use format filter=value[RED]')
+        raise ExitProgram
+    except KeyError as e: # Unknown filters
+        echo(f'[RED]Filter "{e.args[0]}" doesn\'t exists[RED]')
+        raise ExitProgram
 
-    if entity:
-        for e in entity:
-            drbf = tgbox.sync(drb.get_file(id))
-            if not drbf:
-                echo(f'[RED]File with ID={id} doesn\'t exist in RemoteBox[RED]')
-                raise ExitProgram
-            try:
-                tgbox.sync(drb.tc.forward_messages(e, drbf.message))
-                echo(f'[GREEN]ID={id} was forwarded to {e}[GREEN]')
-            except (UsernameNotOccupiedError, UsernameInvalidError, ValueError):
-                echo(f'[YELLOW]Can not find entity "{e}"[YELLOW]')
-    else:
-        echo('[RED]At least one entity should be specified[RED]')
+    to_forward = dlb.search_file(sf)
+
+    for dlbf in sync_async_gen(to_forward):
+        drbf = tgbox.sync(drb.get_file(dlbf.id))
+        try:
+            tgbox.sync(drb.tc.forward_messages(entity, drbf.message))
+            echo(f'[GREEN]ID{drbf.id}: {drbf.file_name}: was forwarded to {entity}[GREEN]')
+        except (UsernameNotOccupiedError, UsernameInvalidError, ValueError):
+            echo(f'[YELLOW]Can not find entity "{entity}"[YELLOW]')
 
     raise ExitProgram
 
