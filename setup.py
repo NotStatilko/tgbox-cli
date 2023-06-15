77ff2973e13d965933c511b23edfbfed283f635c
NotStatilko <thenonproton@protonmail.com>
2023-06-16 02:37:24 +0300

    Bump enlighten to v1.11.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'urllib3',
         'tgbox<2',
         'click==8.1.3',
-        'enlighten==1.10.2'
+        'enlighten==1.11.2'
     ],
     extras_require={
         'fast': ['tgbox[fast]<2']
